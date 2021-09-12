74eeb0bb75fee3ed67a9830257122470ccde9d9d
Matthew Sievers <mattersievers@gmail.com>
2021-09-12 01:10:25 -0500

    added intial files

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
         password:'v3g3t@bl3H0+r@+$',
         database:'employees'
     },
-    console.log('Connected to the election database.')
+    console.log('Connected to the employee database.')
 );
 
 module.exports = db;
